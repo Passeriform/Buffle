ec583ec177089cfa6ee1350ef57ff7939f70d5e5
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-02-16 06:53:43 +0530

    [Lint] Add ESLint and Prettier plugin

---
+++ eslint.config.ts
@@ -0,0 +1,44 @@
+import antfu from "@antfu/eslint-config"
+
+export default antfu({
+    ignores: ["tsconfig.json"],
+    formatters: {
+
+    },
+    imports: {
+        overrides: {
+            "import/consistent-type-specifier-style": "off",
+        },
+    },
+    unicorn: {
+        overrides: {
+            "unicorn/number-literal-case": ["error", { hexadecimalValue: "lowercase" }],
+        },
+    },
+    stylistic: {
+        quotes: "double",
+        indent: 4,
+        overrides: {
+            "style/arrow-parens": ["error", "always"],
+            "style/brace-style": ["error", "1tbs"],
+            "style/quote-props": ["error", "as-needed"],
+            "style/operator-linebreak": ["error", "before", { overrides: { "=": "after" } }],
+        },
+    },
+    typescript: {
+        overrides: {
+            "ts/consistent-type-definitions": ["error", "type"],
+        },
+    },
+    yaml: {
+        overrides: {
+            "yaml/indent": ["error", 2],
+        },
+    },
+    rules: {
+        "id-length": ["error", { exceptions: ["_", "x", "y"] }],
+        "prefer-template": "off",
+        "antfu/top-level-function": "off",
+        "node/prefer-global/process": "off",
+    },
+})
